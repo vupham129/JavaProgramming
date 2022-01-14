ee666ca417def83d3dcd0b89f3d99f5098ed8959
vupha <vupham129@gmail.com>
2022-01-14 08:05:49 -0800

    day39_Recap

--- src/day39_Recap/shapeTask/Shape.java
+++ src/day39_Recap/shapeTask/Shape.java
@@ -35,6 +35,8 @@ public class Shape {
     public String toString() {
         return "Shape{" +
                 "name='" + name + '\'' +
+                ", area='" + area() + '\'' +
+                ", perimeter='" + perimeter() + '\'' +
                 '}';
     }
 }
