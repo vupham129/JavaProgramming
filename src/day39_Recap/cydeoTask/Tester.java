b0774cb02ebf10ae54266fc1ec43b1c7bca17540
vupha <vupham129@gmail.com>
2022-01-14 12:59:57 -0800

    day39_Recap

--- src/day39_Recap/cydeoTask/Tester.java
+++ src/day39_Recap/cydeoTask/Tester.java
@@ -1,4 +1,19 @@
 package day39_Recap.cydeoTask;
 
-public class Tester {
+public class Tester extends Employee{
+    public Tester(String name, int age, char gender, int employeeID, String jobTitle, double salary) {
+        super(name, age, gender, employeeID, jobTitle, salary);
+    }
+    public void createTicket(){
+        System.out.println(getName()+" "+getJobTitle()+" is creating ticket");
+    }
+
 }
+/*
+  3. Create a sub class of Employee named Tester
+
+            Override the work method
+
+            Extra methods:
+                createTicket()
+ */
