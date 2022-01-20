44ad57f46d64dc39202d5bc8023ca741156cd301
vupha <vupham129@gmail.com>
2022-01-20 07:47:42 -0800

    day40-FinalKeyword

---
+++ src/day40_FinalKeyword/ProtectedAccessModifier.java
@@ -0,0 +1,11 @@
+package day40_FinalKeyword;
+
+public class ProtectedAccessModifier {
+
+    static String name1 = "Cydeo"; // default access modifier
+
+    protected static String name2 = "WoodenSpoon"; // protected
+
+
+
+}
---
+++ src/utilities/AccessModifier_Test3.java
@@ -0,0 +1,4 @@
+package utilities;
+
+public class AccessModifier_Test3 {
+}
