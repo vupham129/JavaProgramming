649b7b0251364db984c8ac749bed1cece689a518
vupha <vupham129@gmail.com>
2022-01-21 19:09:07 -0800

    day41_Exception

---
+++ src/day41_Exception/FinallyBlock.java
@@ -0,0 +1,30 @@
+package day41_Exception;
+
+public class FinallyBlock {
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,3};
+
+        try {
+            System.out.println(arr[10]);
+            System.out.println("Try Block");
+        }catch (RuntimeException e){
+            System.out.println("Catch Block");
+            e.printStackTrace();
+            System.exit(0);// will terminate the program so finally block will not be executed
+        }finally {
+            System.out.println("Finally Block");
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
