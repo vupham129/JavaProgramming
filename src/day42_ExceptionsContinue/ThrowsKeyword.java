9b0be32ae5f842c5919da8b966e93a9615bd0d18
vupha <vupham129@gmail.com>
2022-01-27 23:08:51 -0800

    day42_ExceptionContinues

---
+++ src/day42_ExceptionsContinue/ThrowsKeyword.java
@@ -0,0 +1,48 @@
+package day42_ExceptionsContinue;
+
+public class ThrowsKeyword {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println("-----------Test 1----------------");
+
+        System.out.println("Hello");
+
+        Thread.sleep(3000);
+
+        System.out.println("Cydeo");
+
+        System.out.println("---------Test 1 completed-----------");
+
+        System.out.println("-----------Test 2----------------");
+
+        System.out.println("Hello");
+
+        Thread.sleep(3000);
+
+        System.out.println("Batch 25");
+
+        System.out.println("---------Test 2 completed-----------");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
