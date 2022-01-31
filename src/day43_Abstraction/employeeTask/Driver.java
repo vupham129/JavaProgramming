d3649363169c80cb95027ad15dd70a2598eb0cbd
vupha <vupham129@gmail.com>
2022-01-31 13:48:21 -0800

    day43_Abstraction

--- src/day43_Abstraction/employeeTask/Driver.java
+++ src/day43_Abstraction/employeeTask/Driver.java
@@ -1,6 +1,6 @@
 package day43_Abstraction.employeeTask;
 
-public class Driver extends Employee{
+public final class Driver extends Employee{
     public Driver(String name, int age, char gender, int id, String jobTitle, double salary) {
         super(name, age, gender, id, jobTitle, salary);
     }
