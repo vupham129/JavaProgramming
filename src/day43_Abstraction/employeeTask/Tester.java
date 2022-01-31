85b208d6ec98f007573fd09cef39ffa2df1edb59
vupha <vupham129@gmail.com>
2022-01-31 13:36:22 -0800

    day43_Abstraction

---
+++ src/day43_Abstraction/employeeTask/Tester.java
@@ -0,0 +1,25 @@
+package day43_Abstraction.employeeTask;
+
+public final class Tester extends Employee {
+
+
+    public Tester(String name, int age, char gender, int id, double salary) {
+        super(name, age, gender, id, "Tester", salary);
+    }
+
+    @Override
+    public void work() {
+        System.out.println(getName()+" is testing the application");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println(getName()+" sleep 8 hours");
+    }
+
+    public void bugReport(){
+        System.out.println(getName()+" is creating bug reports");
+    }
+
+
+}
