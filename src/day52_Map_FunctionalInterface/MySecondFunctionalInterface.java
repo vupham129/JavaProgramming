31a385eb96b425424bb9dbecd379ca9fa7d11df5
vupha <vupham129@gmail.com>
2022-03-03 16:44:51 -0800

    day52_Map_FunctionalInterface

--- src/day52_Map_FunctionalInterface/MySecondFunctionalInterface.java
+++ src/day52_Map_FunctionalInterface/MySecondFunctionalInterface.java
@@ -1,4 +1,8 @@
 package day52_Map_FunctionalInterface;
 
-public interface MySecondFunctionalInterface {
+@FunctionalInterface
+public interface MySecondFunctionalInterface<T> {
+
+    void test(T data);
+
 }
